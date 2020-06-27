c213f681766d3ac0a23b24b701070fc5d06a1d41
michaellux <michaellux@yandex.ua>
2020-06-27 20:58:37 +0300

    добавлены eslint и styleint пакеты

--- webpack.config.js
+++ webpack.config.js
@@ -1,101 +1,106 @@
-const path = require("path");
-const fs = require("fs");
+/* eslint linebreak-style: ["error", "windows"] */
 
+const path = require('path');
+const fs = require('fs');
+const cssnano = require('cssnano');
 const StylelintPlugin = require('stylelint-webpack-plugin');
-
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 function generateHtmlPlugins(templateDir) {
-    const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
-    return templateFiles.map((item) => {
-        const parts = item.split(".");
-        const name = parts[0];
-        const extension = parts[1];
-        return new HtmlWebpackPlugin({
-            filename: `${name}.html`,
-            template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
-            inject: false,
-        });
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+  return templateFiles.map((item) => {
+    const parts = item.split('.');
+    const name = parts[0];
+    const extension = parts[1];
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+      inject: false,
     });
+  });
 }
 
-const htmlPlugins = generateHtmlPlugins("./src/html/views");
+const htmlPlugins = generateHtmlPlugins('./src/html/views');
 
 module.exports = {
-    entry: [
-        "./src/js/index.js", "./src/scss/style.scss"
-    ],
-    output: {
-        filename: './js/bundle.js'
-    },
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src/js'),
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.(sass|scss)$/,
-                include: path.resolve(__dirname, "src/scss"),
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {},
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true,
-                            url: false,
-                        },
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            ident: "postcss",
-                            sourceMap: true,
-                            plugins: () => [
-                                require("cssnano")({
-                                    preset: [
-                                        "default",
-                                        {
-                                            discardComments: {
-                                                removeAll: true,
-                                            },
-                                        },
-                                    ],
-                                }),
-                            ],
-                        },
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            sourceMap: true,
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.html$/,
-                include: path.resolve(__dirname, "src/html/includes"),
-                use: ["raw-loader"],
-            },
+  entry: [
+    './src/js/index.js', './src/scss/style.scss',
+  ],
+  output: {
+    filename: './js/bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        exclude: /(node_modules|bower_components)/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+        {
+          loader: 'eslint-loader',
+        },
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "./css/style.bundle.css",
-        }),
-        new StylelintPlugin(),
-    ].concat(htmlPlugins),
+      },
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              url: false,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              sourceMap: true,
+              plugins: () => [
+                cssnano({
+                  preset: [
+                    'default',
+                    {
+                      discardComments: {
+                        removeAll: true,
+                      },
+                    },
+                  ],
+                }),
+              ],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        include: path.resolve(__dirname, 'src/html/includes'),
+        use: ['raw-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/style.bundle.css',
+    }),
+    new StylelintPlugin(),
+  ].concat(htmlPlugins),
 };
