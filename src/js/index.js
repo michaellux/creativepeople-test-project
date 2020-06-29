1f27183892a7ddd694593f93c65eefa0d1d76c16
michaellux <michaellux@yandex.ua>
2020-06-29 19:01:43 +0300

    создал черновые vue-компоненты

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 import 'bootstrap';
+import Vue from 'vue';
+
+// eslint-disable-next-line no-unused-vars
+const advantages = new Vue({
+  el: '#advantages',
+  data() {
+    return {
+      advantages: 'Этот текст сгенерирован в блоке advantages при помощи Vue',
+    };
+  },
+});
+// eslint-disable-next-line no-unused-vars
+const news = new Vue({
+  el: '#news',
+  data() {
+    return {
+      news: 'Этот текст сгенерирован в блоке news при помощи Vue',
+    };
+  },
+});
