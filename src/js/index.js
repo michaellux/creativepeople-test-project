f24858741d3c57a979287cd697b2fa9f6300c005
michaellux <michaellux@yandex.ua>
2020-06-29 23:06:12 +0300

    каждый компонент поместил в отдельный файл

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,5 @@
+/* eslint-disable no-unused-vars */
 import 'bootstrap';
 import Vue from 'vue';
-
-// eslint-disable-next-line no-unused-vars
-const advantages = new Vue({
-  el: '#advantages',
-  data() {
-    return {
-      advantages: 'Этот текст сгенерирован в блоке advantages при помощи Vue',
-    };
-  },
-});
-// eslint-disable-next-line no-unused-vars
-const news = new Vue({
-  el: '#news',
-  data() {
-    return {
-      news: 'Этот текст сгенерирован в блоке news при помощи Vue',
-    };
-  },
-});
+import Advantages from './components/Advantages';
+import News from './components/News';
