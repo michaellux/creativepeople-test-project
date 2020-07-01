018c8b63aa87c2008849fd5f62d2448eb74a61ae
michaellux <michaellux@yandex.ua>
2020-07-01 21:17:50 +0300

    перенёс слайдер обратно: проблемы с импортом

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
 /* eslint-disable no-unused-vars */
 import 'bootstrap';
 import Vue from 'vue';
-import Slider from './components/Slider';
+import Swiper from 'swiper';
 import Advantages from './components/Advantages';
 import News from './components/News';
+
+// eslint-disable-next-line no-unused-vars
+const mySwiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.figure-about__nextbutton',
+    prevEl: '.figure-about__prevbutton',
+  },
+});
