fe8f90a6898beced63d9d544d4203392ed08b23a
michaellux <michaellux@yandex.ua>
2020-07-05 21:31:27 +0300

    добавил плавный скролл для навигации

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,31 @@ import About from './components/About';
 import Advantages from './components/Advantages';
 import News from './components/News';
 
+const nav = new Vue({
+  el: '#navigation',
+  data() {
+    return {
+      items: [
+        {
+          name: 'Основная информация',
+          link: '#about',
+        },
+        {
+          name: 'Новости',
+          link: '#news',
+        },
+      ],
+    };
+  },
+  methods: {
+    goToBlock(event) {
+      event.preventDefault();
+      const link = event.target.getAttribute('href');
+      document.querySelector(link).scrollIntoView({ behavior: 'smooth', block: 'start' });
+    },
+  },
+});
+
 const foryear = new Vue({
   el: '#footer',
 });
